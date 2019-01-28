f61ccca0ba0248c86014ac0d25f0e031d2ad217e
Edwin Coronado <cor14148@uvg.edu.gt>
2019-01-28 08:30:02 -0600

    Pruba primer boton

--- app/src/main/java/com/example/edwincoronado/audiomanager/AudioMain.kt
+++ app/src/main/java/com/example/edwincoronado/audiomanager/AudioMain.kt
@@ -1,8 +1,16 @@
 package com.example.edwincoronado.audiomanager
 
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.media.AudioManager.*
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+
 
 class AudioMain : AppCompatActivity() {
 
@@ -16,7 +24,17 @@ class AudioMain : AppCompatActivity() {
         val btnring= findViewById<Button>(R.id.btnRing)
         val btnsilencio= findViewById<Button>(R.id.btnSilencio)
         val btnvibrar= findViewById<Button>(R.id.btnVibrar)
-        
+
+        val myAudioManager:AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        btnring.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View): Unit {
+                // Handler code here.
+                myAudioManager.ringerMode
+                Toast.makeText(getBaseContext(), "Se ha colocado en modo sonido", Toast.LENGTH_LONG).show()
+
+            }
+        })
 
     }
 }
